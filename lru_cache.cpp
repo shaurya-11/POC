6574ebc38ff7b9e675d467fae3928686f2cee173
Shaurya Singh <shauryasingh@Shauryas-MacBook-Air.local>
2026-06-09 15:08:44 +0530

    syntax error

--- lru_cache.cpp
+++ lru_cache.cpp
@@ -50,4 +50,4 @@ int main() {
   std::cout << cache.get("one") << "\n";
   std::cout << cache.get("three") << "\n";
 }
-some random gibberish
+some random gibberish to test the cache eviction policy.
